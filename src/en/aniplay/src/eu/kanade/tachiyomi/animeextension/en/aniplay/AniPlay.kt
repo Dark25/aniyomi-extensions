6eae2904830376cf557fab8e76b698f62ba78175
Dark25 <nadiecaca2000@gmail.com>
2024-12-10 23:08:05 +0100

    fixed final

--- src/en/aniplay/src/eu/kanade/tachiyomi/animeextension/en/aniplay/AniPlay.kt
+++ src/en/aniplay/src/eu/kanade/tachiyomi/animeextension/en/aniplay/AniPlay.kt
@@ -456,7 +456,7 @@ class AniPlay : AniListAnimeHttpSource(), ConfigurableAnimeSource {
         private const val PREF_DOMAIN_DEFAULT = "aniplaynow.live"
 
         private const val PREF_SERVER_KEY = "server"
-        private val PREF_SERVER_ENTRIES = arrayOf("Kuro", "Yuki", "Yuno","Anya")
+        private val PREF_SERVER_ENTRIES = arrayOf("Kuro", "Yuki", "Yuno", "Anya")
         private val PREF_SERVER_ENTRY_VALUES = arrayOf("kuro", "yuki", "yuno", "anya")
         private const val PREF_SERVER_DEFAULT = "kuro"
 
